848ec9d1f2f6c9b9908b233218e0d288681ba5f3
Stig <s186333@student.dtu.dk>
2022-04-26 16:59:27 +0200

    added entailment

--- Utils.py
+++ Utils.py
@@ -5,4 +5,51 @@ from operator import neg
 from sympy import to_cnf, SympifyError
 
 
+def removeAll(item, seq):
+    """ Return a copy of seq (or string) with all occurrences of item removed.
+    """
+    if isinstance(seq, str):
+        return seq.replace(item, '')
+    else:
+        return [x for x in seq if x != item]
+    
+def unique(seq):
+    """Remove duplicate elements from seq. Assumes hashable elements."""
+    return list(set(seq))
 
+def associate(op, args):
+    
+    args = dissociate(op, args)
+    if len(args) == 0:
+        return op.identity
+    elif len(args) == 1:
+        return args[0]
+    else:
+        return op(*args)
+    
+    
+def conjuncts(args):
+    return dissociate("&", [args])
+
+def disjuncts(args):
+    return dissociate("|", [args])
+    
+def dissociate(op, args):
+    """given an associative operator and a list of arguments, return a flattened list
+    Args:
+        op (string): like "&" or "|"
+        args (array): [A & B]
+
+    Returns:
+        array: [A, B]
+    """
+    result = []
+    
+    def collect(subargs):
+        for args in subargs:
+            if args.op == op:
+                collect(args.args)
+            else:
+                result.append(args) 
+    collect(args)    
+    return result
---
+++ entailment.py
@@ -0,0 +1,55 @@
+from sympy import to_cnf
+
+from Utils import conjuncts, associate, disjuncts, removeAll, unique
+
+
+def entailment(kb, formula):
+    """
+    Check entailment of a formula in a knowledge base.
+    """
+    formula = to_cnf(formula)
+    clauses = []
+    for i in kb:
+        clauses += conjuncts(i)
+    
+    pl_resolution(clauses, formula)
+    
+         
+    
+    
+    
+def pl_resolution(kb, alpha):
+    """[figure 7.12] from the book.
+    Propositional-logic resolution: say if a KB entails a given alpha. [Figure 7.12]
+
+    Args:
+        kb (knowlageBase): horn clause knowledge base
+        alpha: clause
+    """
+    clauses = kb.clauses + conjuncts(to_cnf(~alpha))
+    new = set()
+    
+    while True:
+        n = len(clauses)
+        pairs = [(clauses[i], clauses[j])
+                 for i in range(n) for j in range(i+1, n)]
+        for (ci, cj) in pairs:
+            resolvents = pl_resolve(ci, cj)
+            if False in resolvents:
+                return True
+            new = new.union(set(resolvents))
+        if new.issubset(set(clauses)):
+            return False
+        for i in new:
+            if i not in clauses:
+                clauses.append(i)
+            
+
+def pl_resolve(ci, cj):
+    """returns the set of all possible clauses obtained by resolving its two inputs."""
+    clauses = []
+    for di in disjuncts(ci):
+        for dj in disjuncts(cj):
+            if di == ~dj or ~di == dj:
+                clauses.append(associate('|', unique(removeAll(di, disjuncts(ci)) + removeAll(dj, disjuncts(cj)))))
+    return clauses
