0e4ad3ff31004e2bc112bd2a3ed19d83626240d8
Kageschwump <kasperfalch@live.dk>
2022-05-01 19:37:22 +0200

    added clear after every command

--- Cli.py
+++ Cli.py
@@ -5,13 +5,14 @@ from sympy import to_cnf, SympifyError
 
 from BeliefBase import BeliefBase
 
-## from belief_base import BeliefBase
 
+## from belief_base import BeliefBase
 
 
 def printHelp():
     print(
-    """
+        """
+    Belief Revision Agent - Group 45
     available commands:
     r: Belief revison
     e: Empty belief base
@@ -19,6 +20,14 @@ def printHelp():
     h: Print help dialog
     q: Quit    
     """)
+
+
+def clearConsole():
+    print('\n' * 150)
+
+
+
+
 
 def handleInput(beliefBase):
     command = input("Select command: ")
@@ -28,38 +37,46 @@ def handleInput(beliefBase):
         formula = input("Please enter formula: ")
         try:
             formula = to_cnf(formula)
-            print (formula)
+            print(formula)
             order = input("Please enter order (real number from 0 to 1): ")
             beliefBase.revise(formula, float(order))
+            clearConsole()
+            printHelp()
         except SympifyError:
             print("Formula is not valid")
         except ValueError:
             print("Order is not valid")
-          
-        
+
+
     elif command == "e":
         print("Empty belief base")
         beliefBase.empty()
-        
-    elif command == "p":
-        print("Print belief base")
-        print(beliefBase)
-        
-    elif command == "h":
+        clearConsole()
         printHelp()
-        
+
+    elif command == "p":
+        clearConsole()
+        print("belief base:")
+        print(beliefBase)
+        print("\n")
+
+
+    elif command == "h":
+        clearConsole()
+        printHelp()
+
     elif command == "q":
         print("Quitting")
         exit()
-        
+
     else:
+        clearConsole()
         print("Unknown command. Type 'h' for help")
-    
+
     handleInput(beliefBase)
 
 
 if __name__ == "__main__":
-    
     beliefBase = BeliefBase()
     printHelp()
     handleInput(beliefBase)
